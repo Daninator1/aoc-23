647782059f66de3065e8ced0c52505547d278aa6
Daniel Zauner <daniel.zauner@admiral.at>
2023-12-19 11:14:56 +0100

    solve day 13 part 2

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -18,6 +18,16 @@ impl Pattern {
         }, |v| v * 100)
     }
 
+    fn get_reflection_value_2(&self) -> usize {
+        Pattern::get_reflection_value_inner_2(&self.grid).map_or_else(|| {
+            let rotated_grid: Vec<Vec<char>> = (0..self.grid[0].len()).map(|i| {
+                self.grid.iter().rev().map(|line| line[i]).collect()
+            }).collect();
+
+            Pattern::get_reflection_value_inner_2(&rotated_grid).unwrap()
+        }, |v| v * 100)
+    }
+
 
     fn get_reflection_value_inner(grid: &Vec<Vec<char>>) -> Option<usize> {
         let duplicate_line_pairs: Vec<(usize, &[Vec<char>])> = grid.windows(2).enumerate().filter(|(_i, lines)| lines.iter().all_equal()).collect();
@@ -27,9 +37,6 @@ impl Pattern {
             let mut lower_idx = duplicate_line_pair.0 + 1;
 
             let mut is_mirrored = true;
-
-            dbg!(&upper_idx);
-            dbg!(&lower_idx);
 
             while is_mirrored {
                 if upper_idx == 0 { break; }
@@ -49,12 +56,72 @@ impl Pattern {
                 }
             }
 
-            dbg!(&is_mirrored);
-
             if is_mirrored { return Some(duplicate_line_pair.0 + 1); }
         }
 
         None
+    }
+
+    fn get_reflection_value_inner_2(grid: &Vec<Vec<char>>) -> Option<usize> {
+        let duplicate_line_pairs: Vec<(usize, bool)> = grid.windows(2).enumerate().filter_map(|(i, lines)| {
+            match Pattern::get_difference(lines) {
+                0 => Some((i, false)),
+                1 => Some((i, true)),
+                _ => None,
+            }
+        }).collect();
+
+        for duplicate_line_pair in duplicate_line_pairs {
+            let mut upper_idx = duplicate_line_pair.0;
+            let mut lower_idx = duplicate_line_pair.0 + 1;
+
+            let mut smudge_used = duplicate_line_pair.1;
+            let mut is_mirrored = true;
+
+            while is_mirrored {
+                if upper_idx == 0 { break; }
+                if lower_idx == grid.len() - 1 { break; }
+
+                upper_idx -= 1;
+                lower_idx += 1;
+
+                if let Some(upper) = grid.get(upper_idx) {
+                    if let Some(lower) = grid.get(lower_idx) {
+                        match Pattern::get_difference(&[upper.clone(), lower.clone()]) {
+                            0 => {
+                                is_mirrored = true;
+                            }
+                            1 => {
+                                is_mirrored = !smudge_used;
+                                smudge_used = true;
+                            }
+                            _ => { is_mirrored = false; }
+                        }
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            if is_mirrored && smudge_used { return Some(duplicate_line_pair.0 + 1); }
+        }
+
+        None
+    }
+
+    fn get_difference(lines: &[Vec<char>]) -> usize {
+        let first = lines.first().unwrap();
+        let second = lines.last().unwrap();
+
+        let mut difference: usize = 0;
+
+        for i in 0..first.len() {
+            if first[i] != second[i] { difference += 1; }
+        }
+
+        difference
     }
 }
 
@@ -75,16 +142,19 @@ pub fn part_one(input: &str) -> Option<usize> {
 
     let patterns = blocks.map(|block| Pattern::from_str(block).unwrap());
 
-    let result = patterns.enumerate().map(|(i, p)| {
-        dbg!("{}", &i);
-        p.get_reflection_value()
-    }).sum();
+    let result = patterns.map(|p| p.get_reflection_value()).sum();
 
     Some(result)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let blocks = input.split("\n\n");
+
+    let patterns = blocks.map(|block| Pattern::from_str(block).unwrap());
+
+    let result = patterns.map(|p| p.get_reflection_value_2()).sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -100,6 +170,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(400));
     }
 }
