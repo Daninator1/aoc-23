7babaa65bb5b6a6878d1aad5281d92c168b34260
Daniel Zauner <daninator.zauner@gmail.com>
2023-12-23 18:18:57 +0100

    solve day 17

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,178 +1,99 @@
 use std::str::FromStr;
-use rayon::prelude::{*};
+use pathfinding::matrix::{directions, Matrix};
+use pathfinding::prelude::astar;
 advent_of_code::solution!(17);
 
 #[derive(Debug)]
 struct Map {
-    grid: Vec<Vec<usize>>,
-    width: usize,
-    height: usize,
+    grid: Matrix<usize>,
 }
 
 #[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
-struct Position {
-    x: usize,
-    y: usize,
-}
-
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
+struct State {
+    position: (usize, usize),
+    direction: (isize, isize),
+    distance: usize,
 }
 
 impl FromStr for Map {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let grid: Vec<Vec<usize>> = s.lines().map(|line| line.chars().map(|c| c.to_digit(10).unwrap() as usize).collect()).collect();
-        Ok(Map { grid, width: s.lines().next().unwrap().chars().count(), height: s.lines().count() })
+        let grid = s.lines().map(|line| line.chars().map(|c| c.to_digit(10).unwrap() as usize)).collect();
+        Ok(Map { grid })
     }
 }
 
-fn calc(map: &Map, current: Position, finish: &Position) -> usize {
-    *calc_rec(map, (current, Direction::Right), finish, 0, &[]).iter().min().unwrap_or(&0)
+fn get_surrounding_states<const MAX_DIST: usize>(state: &State, map: &Map, start: &State) -> Vec<(State, usize)> {
+    [directions::N, directions::E, directions::S, directions::W]
+        .iter()
+        .flat_map(|direction| {
+            // get all neighboring points within the grid
+            map.grid.move_in_direction(state.position, *direction)
+                .map(|position| (position, *direction, *map.grid.get(position).expect("weight at position must exist")))
+        })
+        .filter(|(position, direction, _)| {
+            // do not allow directions that would backtrack
+            let is_direction_inverse = state.direction.0 == -direction.0 && state.direction.1 == -direction.1;
+            !is_direction_inverse && *position != start.position
+        })
+        .flat_map(|(position, direction, weight)| {
+            // return successors if they are within the max allowed distance
+            let distance = match state.direction == direction {
+                true => state.distance + 1,
+                false => 1,
+            };
+
+            match distance <= MAX_DIST {
+                true => {
+                    let next_state = State { position, direction, distance };
+                    Some((next_state, weight))
+                }
+                false => None,
+            }
+        })
+        .collect::<Vec<_>>()
 }
 
-fn calc_rec(map: &Map, current: (Position, Direction), finish: &Position, straight_count: usize, history: &[Position]) -> Vec<usize> {
-    if &current.0 == finish {
-        return vec!(map.grid[current.0.y][current.0.x]);
+fn get_next_state(state: &State, map: &Map) -> Vec<(State, usize)> {
+    match map.grid.move_in_direction(state.position, state.direction) {
+        Some(position) => {
+            let weight = *map.grid.get(position).expect("weight at position must exist");
+            let new_state = State { position, direction: state.direction, distance: state.distance + 1 };
+            vec!((new_state, weight))
+        }
+        None => vec!(),
     }
-
-    let mut new_history = history.to_vec();
-    new_history.push(current.0);
-
-    get_next_positions(current, straight_count, map, history).iter().flat_map(|(position, direction, straight_count)| {
-        calc_rec(map, (*position, *direction), finish, *straight_count, &new_history)
-            .iter()
-            .map(|x| map.grid[current.0.y][current.0.x] + *x)
-            .collect::<Vec<usize>>()
-    }).collect()
 }
 
-fn get_next_positions(current: (Position, Direction), straight_count: usize, map: &Map, history: &[Position]) -> Vec<(Position, Direction, usize)> {
-    let (position, direction) = current;
+fn calc<const MIN_DIST: usize, const MAX_DIST: usize>(map: Map, start: State, finish_pos: (usize, usize)) -> usize {
+    let path = astar(
+        &start,
+        |state| match state.distance >= MIN_DIST || (state.direction == (0, 0)) {
+            true => get_surrounding_states::<MAX_DIST>(state, &map, &start),
+            false => get_next_state(state, &map),
+        },
+        |state| finish_pos.0.abs_diff(state.position.0) + finish_pos.1.abs_diff(state.position.1),
+        |state| state.position == finish_pos && state.distance >= MIN_DIST,
+    ).expect("one path must be there");
 
-    match direction {
-        Direction::Up => {
-            let mut next = vec!();
-
-            if position.y > 0 && straight_count < 3 {
-                let new_position = Position { x: position.x, y: position.y - 1 };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Up, straight_count + 1));
-                }
-            }
-
-            if position.x < map.width - 1 {
-                let new_position = Position { x: position.x + 1, y: position.y };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Right, 0));
-                }
-            }
-
-            if position.x > 0 {
-                let new_position = Position { x: position.x - 1, y: position.y };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Left, 0));
-                }
-            }
-
-            next
-        }
-        Direction::Down => {
-            let mut next = vec!();
-
-            if position.y < map.height - 1 && straight_count < 3 {
-                let new_position = Position { x: position.x, y: position.y + 1 };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Down, straight_count + 1));
-                }
-            }
-
-            if position.x < map.width - 1 {
-                let new_position = Position { x: position.x + 1, y: position.y };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Right, 0));
-                }
-            }
-
-            if position.x > 0 {
-                let new_position = Position { x: position.x - 1, y: position.y };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Left, 0));
-                }
-            }
-
-            next
-        }
-        Direction::Left => {
-            let mut next = vec!();
-
-            if position.x > 0 && straight_count < 3 {
-                let new_position = Position { x: position.x - 1, y: position.y };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Left, straight_count + 1));
-                }
-            }
-
-            if position.y > 0 {
-                let new_position = Position { x: position.x, y: position.y - 1 };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Up, 0));
-                }
-            }
-
-            if position.y < map.height - 1 {
-                let new_position = Position { x: position.x, y: position.y + 1 };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Down, 0));
-                }
-            }
-
-            next
-        }
-        Direction::Right => {
-            let mut next = vec!();
-
-            if position.x < map.width - 1 && straight_count < 3 {
-                let new_position = Position { x: position.x + 1, y: position.y };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Right, straight_count + 1));
-                }
-            }
-
-            if position.y > 0 {
-                let new_position = Position { x: position.x, y: position.y - 1 };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Up, 0));
-                }
-            }
-
-            if position.y < map.height - 1 {
-                let new_position = Position { x: position.x, y: position.y + 1 };
-                if !history.contains(&new_position) {
-                    next.push((new_position, Direction::Down, 0));
-                }
-            }
-
-            next
-        }
-    }
+    path.1
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
     let map = Map::from_str(input).unwrap();
-    let start = Position { x: 0, y: 0 };
-    let finish = Position { x: map.width - 1, y: map.height - 1 };
-    let result = calc(&map, start, &finish);
+    let start = State { position: (0, 0), direction: (0, 0), distance: 0 };
+    let finish_pos = (map.grid.rows - 1, map.grid.columns - 1);
+    let result = calc::<1, 3>(map, start, finish_pos);
     Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let map = Map::from_str(input).unwrap();
+    let start = State { position: (0, 0), direction: (0, 0), distance: 0 };
+    let finish_pos = (map.grid.rows - 1, map.grid.columns - 1);
+    let result = calc::<4, 10>(map, start, finish_pos);
+    Some(result)
 }
 
 #[cfg(test)]
@@ -188,6 +109,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(71));
     }
 }
