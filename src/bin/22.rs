2fac0d0f99fcb6a5ed90b28551651c9b98029199
Daniel Zauner <daniel.zauner@admiral.at>
2024-01-05 11:14:35 +0100

    solve day 22 part 2

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 use itertools::Itertools;
 advent_of_code::solution!(22);
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 struct Brick {
     id: usize,
     from: Position,
@@ -23,7 +23,7 @@ impl Brick {
     }
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 struct Position {
     x: usize,
     y: usize,
@@ -48,8 +48,10 @@ fn intersects(this: (usize, usize), other: (usize, usize)) -> bool {
     this.0 <= other.1 && this.1 >= other.0
 }
 
-fn fall(bricks: &[Brick]) -> Vec<Brick> {
+fn fall(bricks: &[Brick]) -> (Vec<Brick>, usize) {
     let mut fallen_bricks: Vec<Brick> = vec!();
+
+    let mut fallen_count = 0;
 
     for brick in bricks {
         let mut updated_z = brick.from.z;
@@ -72,6 +74,8 @@ fn fall(bricks: &[Brick]) -> Vec<Brick> {
 
         let diff_z = brick.from.z - updated_z;
 
+        if diff_z > 0 { fallen_count += 1; }
+
         fallen_bricks.push(Brick {
             id: brick.id,
             from: Position {
@@ -87,7 +91,7 @@ fn fall(bricks: &[Brick]) -> Vec<Brick> {
         })
     }
 
-    fallen_bricks
+    (fallen_bricks, fallen_count)
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
@@ -100,8 +104,8 @@ pub fn part_one(input: &str) -> Option<usize> {
 
     let fallen_bricks = fall(&bricks);
 
-    let structure: Vec<(&Brick, Vec<&Brick>)> = fallen_bricks.iter().map(|b| {
-        let supports = fallen_bricks
+    let structure: Vec<(&Brick, Vec<&Brick>)> = fallen_bricks.0.iter().map(|b| {
+        let supports = fallen_bricks.0
             .iter()
             .filter(|o| o.from.z == b.to.z + 1)
             .filter(|below| intersects((b.from.x, b.to.x), (below.from.x, below.to.x)) &&
@@ -123,8 +127,42 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(result.count())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let bricks: Vec<Brick> = input
+        .lines()
+        .enumerate()
+        .map(|(line_idx, line)| Brick::create(line, line_idx))
+        .sorted_by(|a, b| a.from.z.cmp(&b.from.z))
+        .collect();
+
+    let fallen_bricks = fall(&bricks);
+
+    let structure: Vec<(&Brick, Vec<&Brick>)> = fallen_bricks.0.iter().map(|b| {
+        let supports = fallen_bricks.0
+            .iter()
+            .filter(|o| o.from.z == b.to.z + 1)
+            .filter(|below| intersects((b.from.x, b.to.x), (below.from.x, below.to.x)) &&
+                intersects((b.from.y, b.to.y), (below.from.y, below.to.y)));
+
+        (b, supports.collect())
+    }).collect();
+
+    let removable_bricks = structure.iter().flat_map(|(me, supports)| {
+        if supports.is_empty() { return None; }
+
+        if supports.iter().all(|s| {
+            structure.iter().any(|(x, y)| x.id != me.id && y.contains(s))
+        }) { return None; }
+
+        Some(me)
+    });
+
+    let result = removable_bricks.map(|removable_brick| {
+        let modified_bricks: Vec<Brick> = fallen_bricks.0.iter().cloned().filter(|b| b.id != removable_brick.id).collect();
+        fall(&modified_bricks).1
+    }).sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -140,6 +178,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(7));
     }
 }
