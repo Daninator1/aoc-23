c7ec40fadbce577e5ea729fcf693cab8b1295d5a
Daniel Zauner <daniel.zauner@admiral.at>
2024-01-08 13:14:10 +0100

    day 23 part 2 slow approach

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 use pathfinding::matrix::{directions, Matrix};
+use rayon::prelude::{*};
 advent_of_code::solution!(23);
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
@@ -9,8 +10,8 @@ enum Tile {
     Slope((isize, isize)),
 }
 
-impl From<char> for Tile {
-    fn from(value: char) -> Self {
+impl Tile {
+    fn create_part_1(value: char) -> Self {
         match value {
             '#' => Tile::Forest,
             '.' => Tile::Path,
@@ -21,6 +22,14 @@ impl From<char> for Tile {
             _ => panic!("unexpected tile"),
         }
     }
+
+    fn create_part_2(value: char) -> Self {
+        match value {
+            '#' => Tile::Forest,
+            '.' | '^' | 'v' | '<' | '>' => Tile::Path,
+            _ => panic!("unexpected tile"),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -28,21 +37,25 @@ struct Map {
     grid: Matrix<Tile>,
 }
 
-impl FromStr for Map {
-    type Err = ();
+impl Map {
+    fn create_part_1(s: &str) -> Self {
+        let grid = s.lines().map(|line| line.chars().map(Tile::create_part_1)).collect();
+        Map { grid }
+    }
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let grid = s.lines().map(|line| line.chars().map(Tile::from)).collect();
-        Ok(Map { grid })
+    fn create_part_2(s: &str) -> Self {
+        let grid = s.lines().map(|line| line.chars().map(Tile::create_part_2)).collect();
+        Map { grid }
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 struct State {
     position: (usize, usize),
     direction: (isize, isize),
     distance: usize,
     tile: Tile,
+    visited_positions: Vec<(usize, usize)>,
 }
 
 fn calc(map: Map, start: State, finish_pos: (usize, usize)) -> usize {
@@ -51,7 +64,7 @@ fn calc(map: Map, start: State, finish_pos: (usize, usize)) -> usize {
 
     while !current_states.is_empty() {
         let new_states = current_states
-            .iter()
+            .par_iter()
             .flat_map(|current_state| get_next_states(current_state, &map));
 
         let (new_finished_states, new_unfinished_states): (Vec<_>, Vec<_>) = new_states.partition(|new_state| new_state.position == finish_pos);
@@ -79,36 +92,44 @@ fn get_next_states(state: &State, map: &Map) -> Vec<State> {
             map.grid.move_in_direction(state.position, *direction)
                 .map(|position| (position, *direction, *map.grid.get(position).expect("tile at position must exist")))
         })
-        .filter(|(position, direction, _)| {
+        .filter(|(_, direction, _)| {
             // do not allow directions that would backtrack
             let is_direction_inverse = state.direction.0 == -direction.0 && state.direction.1 == -direction.1;
             !is_direction_inverse
         })
-        .filter(|(position, direction, tile)| {
+        .filter(|(position, _, _)| {
+            // do not allow directions that would hit an already visited tile
+            !state.visited_positions.contains(position)
+        })
+        .filter(|(_, _, tile)| {
             // do not allow directions that would hit a forest or inverting slope
             match tile {
                 Tile::Forest => false,
                 Tile::Path => true,
-                Tile::Slope(slope_direction) => !(slope_direction == &(-state.direction.0, -state.direction.1)),
+                Tile::Slope(slope_direction) => slope_direction != &(-state.direction.0, -state.direction.1),
             }
         })
         .map(|(position, direction, tile)| {
             // return successors
-            State { position, direction, distance: state.distance + 1, tile }
+            State { position, direction, distance: state.distance + 1, tile, visited_positions: state.visited_positions.clone().into_iter().chain(vec!(position)).collect() }
         })
         .collect::<Vec<_>>()
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let map = Map::from_str(input).unwrap();
-    let start = State { position: (0, 1), direction: (1, 0), distance: 0, tile: Tile::Path };
+    let map = Map::create_part_1(input);
+    let start = State { position: (0, 1), direction: (1, 0), distance: 0, tile: Tile::Path, visited_positions: vec!() };
     let finish_pos = (map.grid.rows - 1, map.grid.columns - 2);
     let result = calc(map, start, finish_pos);
     Some(result)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let map = Map::create_part_2(input);
+    let start = State { position: (0, 1), direction: (1, 0), distance: 0, tile: Tile::Path, visited_positions: vec!() };
+    let finish_pos = (map.grid.rows - 1, map.grid.columns - 2);
+    let result = calc(map, start, finish_pos);
+    Some(result)
 }
 
 #[cfg(test)]
@@ -124,6 +145,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(154));
     }
 }
