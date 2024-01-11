5ebbb43e7756474600cc7def6ed2e21da709780e
Daniel Zauner <daniel.zauner@admiral.at>
2024-01-11 13:09:01 +0100

    day 24 part 2 try

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -1,7 +1,6 @@
 use std::str::FromStr;
 use intersect2d::{intersect, Intersection};
 use itertools::Itertools;
-use num::abs;
 advent_of_code::solution!(24);
 
 #[derive(Debug, Copy, Clone)]
@@ -66,8 +65,78 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(intersections.len())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let hailstones = input.lines().map(|line| Hailstone::from_str(line).unwrap());
+
+    let (stone_a, stone_b, stone_c) = hailstones.take(3).collect_tuple().unwrap();
+
+    let ext_2 = [
+        exterior2(&subtract_velocities(&stone_a.velocity, &stone_b.velocity), &subtract_positions(&stone_a.position, &stone_b.position)),
+        exterior2(&subtract_velocities(&stone_a.velocity, &stone_c.velocity), &subtract_positions(&stone_a.position, &stone_c.position)),
+        exterior2(&subtract_velocities(&stone_b.velocity, &stone_c.velocity), &subtract_positions(&stone_b.position, &stone_c.position))
+    ];
+
+    let ext_3 = [
+        -exterior3(&stone_a.position, &stone_a.velocity, &stone_b.position) - exterior3(&stone_b.position, &stone_b.velocity, &stone_a.position),
+        -exterior3(&stone_a.position, &stone_a.velocity, &stone_c.position) - exterior3(&stone_c.position, &stone_c.velocity, &stone_a.position),
+        -exterior3(&stone_b.position, &stone_b.velocity, &stone_c.position) - exterior3(&stone_c.position, &stone_c.velocity, &stone_b.position)
+    ];
+
+    let det_a =
+        ext_2[0].0 * ext_2[1].1 * ext_2[2].2
+            - ext_2[0].0 * ext_2[1].2 * ext_2[2].1
+            - ext_2[0].1 * ext_2[1].0 * ext_2[2].2
+            + ext_2[0].1 * ext_2[1].2 * ext_2[2].0
+            + ext_2[0].2 * ext_2[1].0 * ext_2[2].1
+            - ext_2[0].2 * ext_2[1].1 * ext_2[2].0;
+
+    let det_ax = ext_3[0] * ext_2[1].1 * ext_2[2].2
+        - ext_3[0] * ext_2[1].2 * ext_2[2].1
+        - ext_2[0].1 * ext_3[1] * ext_2[2].2
+        + ext_2[0].1 * ext_2[1].2 * ext_3[2]
+        + ext_2[0].2 * ext_3[1] * ext_2[2].1
+        - ext_2[0].2 * ext_2[1].1 * ext_3[2];
+
+    let det_ay = ext_2[0].0 * ext_3[1] * ext_2[2].2
+        - ext_2[0].0 * ext_2[1].2 * ext_3[2]
+        - ext_3[0] * ext_2[1].0 * ext_2[2].2
+        + ext_3[0] * ext_2[1].2 * ext_2[2].0
+        + ext_2[0].2 * ext_2[1].0 * ext_3[2]
+        - ext_2[0].2 * ext_3[1] * ext_2[2].0;
+
+    let det_az = ext_2[0].0 * ext_2[1].1 * ext_3[2]
+        - ext_2[0].0 * ext_3[1] * ext_2[2].1
+        - ext_2[0].1 * ext_2[1].0 * ext_3[2]
+        + ext_2[0].1 * ext_3[1] * ext_2[2].0
+        + ext_3[0] * ext_2[1].0 * ext_2[2].1
+        - ext_3[0] * ext_2[1].1 * ext_2[2].0;
+
+    let x = det_ax / det_a;
+    let y = det_ay / det_a;
+    let z = det_az / det_a;
+
+    Some((x.ceil() + y.ceil() + z.ceil()) as usize)
+}
+
+fn subtract_velocities(a: &Velocity, b: &Velocity) -> Velocity {
+    Velocity(a.0 - b.0, a.1 - b.1, a.2 - b.2)
+}
+
+fn subtract_positions(a: &Position, b: &Position) -> Position {
+    Position(a.0 - b.0, a.1 - b.1, a.2 - b.2)
+}
+
+fn exterior2(velocity: &Velocity, position: &Position) -> (f64, f64, f64) {
+    (velocity.0 * position.1 - velocity.1 * position.0,
+     velocity.1 * position.2 - velocity.2 * position.1,
+     velocity.2 * position.0 - velocity.0 * position.2, )
+}
+
+fn exterior3(a_pos: &Position, a_vel: &Velocity, b_pos: &Position) -> f64 {
+    a_pos.0 * a_vel.1 * b_pos.2 + a_pos.1 * a_vel.2 * b_pos.0 + a_pos.2 * a_vel.0 * b_pos.1
+        - a_pos.0 * a_vel.2 * b_pos.1
+        - a_pos.1 * a_vel.0 * b_pos.2
+        - a_pos.2 * a_vel.1 * b_pos.0
 }
 
 #[cfg(test)]
@@ -83,6 +152,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(47));
     }
 }
